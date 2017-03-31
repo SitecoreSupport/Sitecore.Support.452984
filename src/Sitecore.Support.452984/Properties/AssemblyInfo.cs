e95ed436cd8fbce97d9601be0aa1335fdf69d3d1
MasoudA <masoud_nitro@yahoo.com>
2017-03-31 18:29:42 +0800

    initial release

---
+++ src/Sitecore.Support.452984/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.452984")]
+[assembly: AssemblyProduct("Sitecore.Support.452984")]
+[assembly: ComVisible(false)]
